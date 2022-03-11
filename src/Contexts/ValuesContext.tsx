6a6d4628650defc98089fd434cae6e9b9f5b7d1a
ArthThapa <arththapa7@gmail.com>
2022-03-11 08:08:29 +0530

    valuesContext

---
+++ src/Contexts/ValuesContext.tsx
@@ -0,0 +1,28 @@
+import { createContext, Dispatch, FC, SetStateAction, useState } from "react";
+
+export interface ValueProps {
+  height: number;
+  weight: number;
+  setHeight: Dispatch<SetStateAction<number>>;
+  setWeight: Dispatch<SetStateAction<number>>;
+}
+
+const valuesContext = createContext<ValueProps>({
+  height: 0,
+  weight: 0,
+  setHeight: () => {},
+  setWeight: () => {},
+});
+
+const ValuesContext: FC = ({ children }) => {
+  const [height, setHeight] = useState<number>(0);
+  const [weight, setWeight] = useState<number>(0);
+
+  return (
+    <valuesContext.Provider value={{ height, setHeight, weight, setWeight }}>
+      {children}
+    </valuesContext.Provider>
+  );
+};
+
+export default ValuesContext;
